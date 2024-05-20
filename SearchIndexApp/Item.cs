b533e5e3c00bb0551e8aa7194f5a23d0e9073fdf
andreasalatino <andrea.salatino@alten.it>
2024-05-20 10:12:18 +0200

    Add project files.

---
+++ SearchIndexApp/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchIndexApp
+{
+	public class Item
+	{
+        public string Id { get; set; }
+        //public Title Title { get; set; }
+        //public Description Description { get; set; }
+        public string Link { get; set; }
+        public string Condition { get; set; }
+        public string Price { get; set; }
+        public string Brand { get; set; }
+        public string Size { get; set; }
+    }
+}
---
+++ SearchIndexApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
+using Azure.Search.Documents.Models;
+using SearchIndexApp;
+using Azure.Search;
+using Microsoft.Azure.Search;
+
+namespace SearchIndexApp {
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			string serviceName = "languageanalios-asq6yt5nl2tq7ro";
+			string apiKey = "emrqcuZl1bfC0fq0joUSzKdgdfdG7FhrM15yqupDspAzSeBQx9Qu";
+			string indexName = "indexitem";
+
+			// Create a SearchIndexClient to send create/delete index commands
+			Uri serviceEndpoint = new Uri($"https://{serviceName}.search.windows.net/");
+			AzureKeyCredential credential = new AzureKeyCredential(apiKey);
+			SearchIndexClient adminClient = new SearchIndexClient(serviceEndpoint, credential);
+
+			// Create a SearchClient to load and query documents
+			SearchClient srchclient = new SearchClient(serviceEndpoint, indexName, credential);
+
+			Console.WriteLine("Test");
+
+			RunQueries(srchclient);
+		}
+
+		// Write search results to console
+		private static void WriteDocuments(SearchResults<Item> searchResults)
+		{
+			foreach (SearchResult<Item> result in searchResults.GetResults())
+			{
+				Console.WriteLine(result.Document);
+			}
+
+			Console.WriteLine();
+		}
+
+		private static void WriteDocuments(AutocompleteResults autoResults)
+		{
+			foreach (AutocompleteItem result in autoResults.Results)
+			{
+				Console.WriteLine(result.Text);
+			}
+
+			Console.WriteLine();
+		}
+
+		// Run queries, use WriteDocuments to print output
+		private static void RunQueries(SearchClient srchclient)
+		{
+			SearchOptions options;
+			SearchResults<Item> response;
+
+			// Query 1
+			Console.WriteLine("Query #1: Search on empty term '*' to return all documents, showing a subset of fields...\n");
+
+			options = new SearchOptions()
+			{
+				IncludeTotalCount = true,
+				Filter = "",
+				OrderBy = { "" }
+			};
+
+			options.Select.Add("id");
+			options.Select.Add("link");
+			options.Select.Add("condition");
+
+			response = srchclient.Search<Item>("*", options);
+			WriteDocuments(response);
+		}
+	}
+}
+
